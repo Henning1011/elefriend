28bd42a113b13f3acae5aa9e87faaf6da15db25e
Adriana Shibata <dri_src@hotmail.com>
2023-11-21 15:27:38 +0100

    seeded 3 users and 3 pets

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,6 +2,6 @@ class Pet < ApplicationRecord
   belongs_to :user
   has_many :bookings
 
-  validates :name, :title, :description, :price, :category, :species, :location, presence: true
+  validates :pet_name, :title, :description, :price, :category, :species, :location, presence: true
   validates :title, length: { minimum: 10 }
 end
