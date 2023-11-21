f5d398089f72d406774ff422221cd813f1d9844c
Adriana Shibata <dri_src@hotmail.com>
2023-11-21 14:45:09 +0100

    models validations ok

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :pet
   belongs_to :user
+
+  validates :date, presence: true
+  validates :message, presence: true
+  validates :message, length: { minimum: 15 }
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,7 @@
 class Pet < ApplicationRecord
   belongs_to :user
   has_many :bookings
+
+  validates :name, :title, :description, :price, :category, :species, :location, presence: true
+  validates :title, length: { minimum: 10 }
 end
