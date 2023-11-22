8a618500e8e0a274aa74518f1ecc09089ab0c54a
Samuel Metz <metz.samuel@web.de>
2023-11-22 16:37:08 +0100

    Added Filter function for categories on index page - with add this time

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,7 +2,8 @@ class Pet < ApplicationRecord
   belongs_to :user
   has_many :bookings
 
-
+  CATEGORY = ["Transport", "Cuddle", "Therapy", "Adventure", "Party", "Relax", "Sports"]
   validates :pet_name, :title, :description, :price, :category, :species, :location, presence: true
   validates :title, length: { minimum: 10 }
+  validates :category, inclusion: { in: CATEGORY }
 end
