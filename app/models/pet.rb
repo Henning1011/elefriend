b0f989778154d474ad91fdbeb7e4b1a2535c2b4c
Adriana Shibata <dri_src@hotmail.com>
2023-11-23 15:43:07 +0100

    pets | images inserted into the seed file and the schema was updated

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,6 +1,6 @@
 class Pet < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
 
   CATEGORY = ["Transport", "Cuddle", "Therapy", "Adventure", "Party", "Relax", "Sports"]
   validates :pet_name, :title, :description, :price, :category, :species, :location, presence: true
