054fc3cee5c660783691e2ef3e6808a402e3de48
Sammy <144249065+metzsa@users.noreply.github.com>
2023-11-23 10:49:57 +0100

    Update bookings_controller.rb
    deleted comments

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -9,7 +9,6 @@ class BookingsController < ApplicationController
     @booking = Booking.new(booking_params)
     @booking.pet = @pet
     @booking.user = current_user
-    # @booking = @pet.bookings.new(booking_params)
     if @booking.save
       redirect_to dashboard_path
     else
@@ -34,19 +33,6 @@ class BookingsController < ApplicationController
   end
 end
 
-
-# def create
-#   @review = Review.new(review_params)
-#   @review.restaurant = @restaurant
-#   @review.save
-#   redirect_to restaurant_path(@restaurant)
-# end
-
-# private
-
-# def set_restaurant
-#   @restaurant = Restaurant.find(params[:restaurant_id])
-# end
 
 # def review_params
 #   params.require(:review).permit(:content)
