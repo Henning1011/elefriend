caabfb10744fdff288e74cc90ad87adc8df50d2e
Sammy <144249065+metzsa@users.noreply.github.com>
2023-11-23 10:48:27 +0100

    Update bookings_controller.rb
    took out raise in create method

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -13,7 +13,6 @@ class BookingsController < ApplicationController
     if @booking.save
       redirect_to dashboard_path
     else
-      raise
       render 'pets/show', status: :unprocessable_entity
     end
   end
