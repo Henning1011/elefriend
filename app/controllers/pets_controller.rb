6e02236ea2c7ff312f2ef3b35d014528ae52dfc6
Henning Stender <henning.stender@icloud.com>
2023-11-23 12:39:58 +0100

    Create New & Create for pets

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,4 +1,5 @@
 class PetsController < ApplicationController
+  # before_action :set_pet, only: %i[create]
   def index
     if params[:category].present?
       @pets = Pet.where(category: params[:category])
@@ -10,5 +11,28 @@ class PetsController < ApplicationController
   def show
     @pet = Pet.find(params[:id])
     @booking = Booking.new
+  end
+
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    @pet = Pet.new(pet_params)
+    @pet.user = current_user
+    if @pet.save
+      redirect_to pet_path(@pet)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+private
+  def set_pet
+    @pet = Pet.find(params[:id])
+  end
+
+  def pet_params
+    params.require(:pet).permit(:pet_name, :title, :description, :price, :category, :species, :location)
   end
 end
